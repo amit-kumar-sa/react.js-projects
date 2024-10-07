20a4426ce9084137ba6acc23c48ac4c942f1d94f
Amit <amitkuswain6@gmail.com>
2024-10-07 15:44:53 +0530

    random color

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 
 import './App.css'
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
 
   return (
-    <>
+    <div className="App">
      {/* Accordian component */}
-     <Accordian/>
-    </>
+     {/* <Accordian/> */}
+     
+     {/* Random color component */}
+     <RandomColor/>
+
+    </div>
   )
 }
 
